d04c722d0a00e2bdf2e93ab287e0c75184248451
Nour Ardhaoui <110036144+nour2003-coder@users.noreply.github.com>
2025-05-29 19:16:03 +0100

    Created 'Maximum Binary Tree' by A2SV Hub

---
+++ 29-May-2025/Maximum Binary Tree 360906.py
@@ -0,0 +1,18 @@
+# Problem: Maximum Binary Tree - https://leetcode.com/problems/maximum-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        if not(nums):
+            return(None)
+        max_=max(nums)
+        index=nums.index(max_)
+        root=TreeNode(max_)
+        root.right=self.constructMaximumBinaryTree(nums[index+1:])
+        root.left=self.constructMaximumBinaryTree(nums[:index])
+        return(root)
