5b9878aba4cd0c9f3c39678ded16740a244dc4a8
Nour Ardhaoui <110036144+nour2003-coder@users.noreply.github.com>
2025-06-13 18:32:47 +0100

    Created 'Fair Distribution of Cookies' by A2SV Hub

---
+++ 13-Jun-2025/Fair Distribution of Cookies 366345.py
@@ -0,0 +1,19 @@
+# Problem: Fair Distribution of Cookies - https://leetcode.com/problems/fair-distribution-of-cookies/
+
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        min_=[float("inf")]
+        def backtrack(l,index):
+            if index == len(cookies):
+                min_[0]=min(min_[0],max(l))
+                return
+            for i in range(k):
+                l[i]+=cookies[index]
+                if max(l)<min_[0]:
+                    backtrack(l,index+1)
+                l[i]-=cookies[index]
+                if l[i] == 0:
+                    break
+        l=[0 for _ in range(k)]
+        backtrack(l,0)
+        return(min_[0])
