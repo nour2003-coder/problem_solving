bb252224c4bfa537841026c0b8d056d1e649df8a
Nour Ardhaoui <110036144+nour2003-coder@users.noreply.github.com>
2025-07-30 09:49:35 +0100

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 30-Jul-2025/Climbing Stairs 380526.py
@@ -0,0 +1,14 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo={}
+        def climb(n):
+            if n <= 2:
+                return n
+            if n not in memo:
+                memo[n]=climb(n-1)+climb(n-2)
+            return memo[n]
+           
+        return climb(n)
+            
