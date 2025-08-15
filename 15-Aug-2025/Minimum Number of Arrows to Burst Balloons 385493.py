8a807ba08252a2b8e71b837cddcc27c14ce17b30
Nour Ardhaoui <110036144+nour2003-coder@users.noreply.github.com>
2025-08-15 18:34:16 +0100

    Created 'Minimum Number of Arrows to Burst Balloons' by A2SV Hub

---
+++ 15-Aug-2025/Minimum Number of Arrows to Burst Balloons 385493.py
@@ -0,0 +1,16 @@
+# Problem: Minimum Number of Arrows to Burst Balloons - https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+
+class Solution(object):
+    def findMinArrowShots(self, points):
+        points.sort(key=lambda x: x[0])
+        arrows = 1
+        end = points[0][1]
+        
+        for balloon in points[1:]:
+            if balloon[0] > end: 
+                arrows += 1  
+                end = balloon[1] 
+            else:
+                end = min(end, balloon[1])
+        
+        return arrows
